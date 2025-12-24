5ffbae009ba27d317f99535382526a4c331e0e79
Ranju Kumari <rjsky009@gmail.com>
2025-12-24 22:30:48 +0530

    Update ReverseString.java

--- Arrays and strings/ReverseString.java
+++ Arrays and strings/ReverseString.java
@@ -1,1 +1,11 @@
+public void reverseString(char[] s) {
 
+        // Swapping the characters at positions i and s.length-1-i in each iterations 
+        // till we reach the middle index of the character array
+        for(int i=0;i<=s.length/2-1;i++){
+            char temp = s[i];
+            s[i]=s[s.length-1-i];
+            s[s.length-1-i] = temp;
+
+        }
+    }
